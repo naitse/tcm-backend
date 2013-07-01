75626a00a85369fb8b9d8988088b32d2f29107ba
adcarabajal <adcarabajal@gmail.com>
2013-07-01 20:02:08 -0300

    Plugins settings get

---
+++ src/main/java/com/mulesoft/tcm/model/Plugin.java
@@ -0,0 +1,34 @@
+package com.mulesoft.tcm.model;
+
+import java.util.HashMap;
+
+public class Plugin {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public HashMap<String, String> getProperties() {
+		return properties;
+	}
+	public void setProperties(HashMap<String, String> properties) {
+		this.properties = properties;
+	}
+	private int id;
+	private String name;
+	private boolean enabled;
+	private HashMap<String, String> properties; 
+}
---
+++ src/main/java/com/mulesoft/tcm/model/PluginsTransformer.java
@@ -0,0 +1,50 @@
+package com.mulesoft.tcm.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageAwareTransformer;
+import org.mule.util.CaseInsensitiveHashMap;
+
+public class PluginsTransformer extends AbstractMessageAwareTransformer{
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public  ArrayList<Plugin> transform(MuleMessage message, String outputEncoding) throws TransformerException {
+		
+		ArrayList<Plugin> plugins = new ArrayList<Plugin>();
+		ArrayList<CaseInsensitiveHashMap> records = (ArrayList<CaseInsensitiveHashMap>) message.getPayload();
+		int currentId = 0;
+		Plugin pluginToAdd = null;
+		
+		for(CaseInsensitiveHashMap row: records){
+			
+			if( currentId != (Integer)row.get("id") ){
+				currentId = (Integer)row.get("id");
+				pluginToAdd = new Plugin();
+				pluginToAdd.setId(currentId);
+				pluginToAdd.setName((String)row.get("name") );
+				pluginToAdd.setEnabled((Boolean)row.get("enabled"));
+				plugins.add(pluginToAdd);
+				
+			}
+			
+		}
+		
+		for(Plugin p: plugins){
+			HashMap<String, String> properties = new HashMap<String, String>();
+			for(CaseInsensitiveHashMap row: records){
+				if((Integer)row.get("id")  == p.getId()){
+					properties.put((String)row.get("prop"), (String)row.get("value"));
+				}
+			}
+			p.setProperties(properties);
+			
+		}
+				
+		return plugins;
+	}
+	
+}
